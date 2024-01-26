bbf9d706957e3880efd5b925d393cb02f8afaf0d
Bakwein <sefatuncaa3@gmail.com>
2024-01-26 20:54:54 +0300

    first_day

---
+++ day1/day1.py
@@ -0,0 +1,47 @@
+# 3 4
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("sefa")
+print("tunca")
+print("turkey")
+print("i am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Sefa','python','Finland']))
+print(type("sefa"))
+print(type('tunca'))
+print(type('''turkey'''))
+
+print()
+
+print(41)
+print(41.41)
+print(41 - 41j)
+#print(41 -j) # ERROR
+print("sefaT")
+print(True)
+print(False)
+print(["l1",True,41,52.12])
+print((32, "st", 52.551, False))
+print({10,40,20,60,57,26,57})
+print(sorted({10,40,20,60,57,26,57}))
+print({"name":"sefa","surname":"tunca"})
+
+print((((8-3)**2)+((10-2)**2))**(1/2))
+
+print(f"{(((8-3)**2)+((10-2)**2))**(1/2):.2f}") #.dan sonra 2 digit
+
+res = (((8-3)**2)+((10-2)**2))**(1/2)
+print(f"{res:.2f}")
+
+
+          
