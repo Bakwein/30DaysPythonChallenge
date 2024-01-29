88cee35c2c225b8d508c74e914258ca973d5bf6a
Bakwein <sefatuncaa3@gmail.com>
2024-01-29 04:20:36 +0300

    day11

---
+++ day11/day11_examples.py
@@ -0,0 +1,33 @@
+def add_two_numbers (num1, num2):
+    total = num1 + num2
+    print(total)
+print(add_two_numbers(num2 = 3, num1 = 2)) # Order does not matter
+
+def generate_full_name (first_name = 'Asabeneh', last_name = 'Yetayeh'):
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+
+print(generate_full_name())
+print(generate_full_name('David','Smith'))
+
+#rastgele eleman sayisi
+def sum_all_nums(*nums):
+    total = 0
+    for num in nums:
+        total += num     # same as total = total + num 
+    return total
+print(sum_all_nums(2, 3, 5)) # 10
+
+def generate_groups (team,*args):
+    print(team)
+    for i in args:
+        print(i)
+print(generate_groups('Team-1','Asabeneh','Brook','David','Eyob'))
+
+
+def square_number (n):
+    return n * n
+def do_something(f, x):
+    return f(x)
+print(do_something(square_number, 3)) # 27
